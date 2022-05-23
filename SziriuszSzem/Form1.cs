0e7223269b873ef832089e2588ba546a77d1c791
Balabill420 <bencebognar20060213@gmail.com>
2022-05-23 19:55:15 +0200

    Yep

---
+++ SziriuszSzem/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SziriuszSzem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true; // this will set it on top of everything, including the taskbar
+            this.FormBorderStyle = FormBorderStyle.None; // remove the title bar
+            this.WindowState = FormWindowState.Maximized; // maximize it to fill the entire screen.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
